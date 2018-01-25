89b3a71ac3d708fd3db5411b55a643ae742b0d34
PANET\Saim <saim@pansoftware.com.au>
2018-01-25 14:03:19 +1100

    Sample project that demonstrates, how to create nuget packages for different platforms and how nuget packages are used in the different projects.

---
+++ ProjectA/ProjectA/BaseService.cs
@@ -0,0 +1,28 @@
+﻿using ProjectB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    public class BaseService
+    {
+        public string GetBaseData()
+        {
+            return "This is from the Base Service";
+        }
+
+        public string GetDerviedData()
+        {
+            var ob = new DerivedService();
+            return ob.GetDerivedData();
+        }
+
+        public string GetNewService()
+        {
+            return "This is newly created Service";
+        }
+    }
+}
---
+++ TestProjectA/TestProjectA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ProjectA;
+
+namespace TestProjectA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new BaseService();
+            Console.WriteLine(obj.GetBaseData());
+            Console.WriteLine(obj.GetDerviedData());
+            Console.WriteLine(obj.GetNewService());
+            Console.ReadLine();
+        }
+    }
+}
